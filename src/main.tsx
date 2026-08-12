8d9af5166b8e8b41edb4f8adc270315336104df8
Isaacava <149443075+Isaacava@users.noreply.github.com>
2026-08-12 23:28:27 +0100

    Replace App component with DeployScreen component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import DeployScreen from './DeployScreen.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <DeployScreen />
   </StrictMode>,
 )
