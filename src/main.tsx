bd39b8cd3a998754f7feb8a51e239eb95e7c5cb1
Isaacava <149443075+Isaacava@users.noreply.github.com>
2026-08-13 01:35:38 +0100

    Refactor main.tsx to use Erc8183Test component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import DeployScreen from './DeployScreen.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Erc8183Test from "./Erc8183Test";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
+  <React.StrictMode>
     <Erc8183Test />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
