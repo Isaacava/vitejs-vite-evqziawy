b14fc0a8d571e4be1dd59ea42c08a7445a1eb2b6
Isaacava <149443075+Isaacava@users.noreply.github.com>
2026-08-14 18:55:32 +0100

    Add agent registration route

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import LandingPage from "./LandingPage";
 import MarketplaceWorkspace from "./MarketplaceWorkspace";
 import MissionConsole from "./MissionConsole";
 import OnchainPrepare from "./OnchainPrepare";
+import AgentRegistration from "./AgentRegistration";
 import "./index.css";
 
 const params = new URLSearchParams(window.location.search);
@@ -12,9 +13,20 @@ const jobId = params.get("job");
 const missionId = params.get("mission");
 const appMode = window.location.pathname === "/app";
 const prepareMode = window.location.pathname === "/prepare";
+const registerMode = window.location.pathname === "/agents/register";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {jobId ? <MissionConsole /> : prepareMode && missionId ? <OnchainPrepare /> : appMode ? <MarketplaceWorkspace /> : <LandingPage />}
+    {jobId ? (
+      <MissionConsole />
+    ) : prepareMode && missionId ? (
+      <OnchainPrepare />
+    ) : registerMode ? (
+      <AgentRegistration />
+    ) : appMode ? (
+      <MarketplaceWorkspace />
+    ) : (
+      <LandingPage />
+    )}
   </React.StrictMode>
 );
