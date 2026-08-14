100e17345d4568bdfa896b07b70773c1d8c7191b
Isaacava <149443075+Isaacava@users.noreply.github.com>
2026-08-14 16:59:17 +0100

    Use stable marketplace dashboard entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import MarketplaceDashboard from "./MarketplaceDashboard";
+import MarketplaceDashboardV4 from "./MarketplaceDashboardV4";
 import Erc8183Test from "./Erc8183Test";
 import ProviderTest from "./ProviderTest";
 import SettlementTest from "./SettlementTest";
@@ -14,7 +14,7 @@ function Root() {
   if (devMode === "client") return <Erc8183Test />;
   if (devMode === "provider") return <ProviderTest />;
   if (devMode === "settlement") return <SettlementTest />;
-  return <MarketplaceDashboard />;
+  return <MarketplaceDashboardV4 />;
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
