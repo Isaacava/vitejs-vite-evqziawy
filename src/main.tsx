ea64b9794ddafc0070512d0348a736e41ea3bc8c
Isaacava <149443075+Isaacava@users.noreply.github.com>
2026-08-14 18:02:01 +0100

    Make landing page the default entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import LandingPage from "./LandingPage";
 import MarketplaceDashboardV4 from "./MarketplaceDashboardV4";
 import MissionConsole from "./MissionConsole";
 import "./index.css";
 
-const jobId = new URLSearchParams(window.location.search).get("job");
+const params = new URLSearchParams(window.location.search);
+const jobId = params.get("job");
+const appMode = window.location.pathname === "/app";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {jobId ? <MissionConsole /> : <MarketplaceDashboardV4 />}
+    {jobId ? <MissionConsole /> : appMode ? <MarketplaceDashboardV4 /> : <LandingPage />}
   </React.StrictMode>
 );
