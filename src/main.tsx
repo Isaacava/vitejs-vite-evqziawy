4fe9b8a7470966d4c7f290267e66dffd318f27c0
Isaacava <149443075+Isaacava@users.noreply.github.com>
2026-08-14 16:59:35 +0100

    Fix Vercel build by removing stale dev harness imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import MarketplaceDashboardV4 from "./MarketplaceDashboardV4";
-import Erc8183Test from "./Erc8183Test";
-import ProviderTest from "./ProviderTest";
-import SettlementTest from "./SettlementTest";
-
 import "./index.css";
-
-const devMode = new URLSearchParams(window.location.search).get("dev");
-
-function Root() {
-  if (devMode === "client") return <Erc8183Test />;
-  if (devMode === "provider") return <ProviderTest />;
-  if (devMode === "settlement") return <SettlementTest />;
-  return <MarketplaceDashboardV4 />;
-}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Root />
+    <MarketplaceDashboardV4 />
   </React.StrictMode>
 );
