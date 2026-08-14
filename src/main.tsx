c4025d9d66793f222c7ffb6b24192f890810de85
Isaacava <149443075+Isaacava@users.noreply.github.com>
2026-08-14 18:31:34 +0100

    Add on-chain mission preparation route

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import ReactDOM from "react-dom/client";
 import LandingPage from "./LandingPage";
 import MarketplaceWorkspace from "./MarketplaceWorkspace";
 import MissionConsole from "./MissionConsole";
+import OnchainPrepare from "./OnchainPrepare";
 import "./index.css";
 
 const params = new URLSearchParams(window.location.search);
 const jobId = params.get("job");
+const missionId = params.get("mission");
 const appMode = window.location.pathname === "/app";
+const prepareMode = window.location.pathname === "/prepare";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {jobId ? <MissionConsole /> : appMode ? <MarketplaceWorkspace /> : <LandingPage />}
+    {jobId ? <MissionConsole /> : prepareMode && missionId ? <OnchainPrepare /> : appMode ? <MarketplaceWorkspace /> : <LandingPage />}
   </React.StrictMode>
 );
