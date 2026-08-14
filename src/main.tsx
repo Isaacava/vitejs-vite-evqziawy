ce8165ae8024a6aa3d098ebb22f07873e2ee1e5f
Isaacava <149443075+Isaacava@users.noreply.github.com>
2026-08-14 17:09:44 +0100

    Wire mission console into application entry

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import MarketplaceDashboardV4 from "./MarketplaceDashboardV4";
+import MissionConsole from "./MissionConsole";
 import "./index.css";
+
+const jobId = new URLSearchParams(window.location.search).get("job");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MarketplaceDashboardV4 />
+    {jobId ? <MissionConsole /> : <MarketplaceDashboardV4 />}
   </React.StrictMode>
 );
