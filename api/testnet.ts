287d3de153155ceb840edbdf36897ccfbd5ee3d6
Isaacava <149443075+Isaacava@users.noreply.github.com>
2026-09-01 02:12:21 +0100

    feat(erc8004): connect federated discovery to Testnet matcher

--- api/testnet.ts
+++ api/testnet.ts
@@ -18,7 +18,7 @@ async function loadHandler(route: string): Promise<Handler | null> {
     case "job-result": return (await import("../server/_testnet/job-result.js")).default as Handler;
     case "job-status": return (await import("../server/_testnet/job-status.js")).default as Handler;
     case "jobs-history": return (await import("../server/_testnet/jobs-history.js")).default as Handler;
-    case "match": return (await import("../server/_testnet/match.js")).default as Handler;
+    case "match": return (await import("../server/_testnet/match-federated.js")).default as Handler;
     case "prepare-quote": return (await import("../server/_testnet/prepare-quote.js")).default as Handler;
     case "providers": return (await import("../server/_testnet/providers.js")).default as Handler;
     case "quotes": return (await import("../server/_testnet/quotes.js")).default as Handler;
